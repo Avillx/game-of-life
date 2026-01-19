3ac22b606b170de0aab786a5de63aa183c694cd2
Avill <for.sv01@gmail.com>
2026-01-19 11:23:31 +0500

    AppDlegate is separated

---
+++ src/appDelegate.js
@@ -0,0 +1,77 @@
+import { GameManager } from "./gameManager.js"
+
+let instance
+
+export class AppDelegate {
+    _resizeObserver
+    _canvas
+    _sizes
+    _gameManager
+
+    constructor() {
+        this._resizeObserver = null
+        this._canvas = null
+        this._gameManager = new GameManager()
+        this._sizes = {
+            rectHeight: 0,
+            rectWidth: 0,
+            hElementsCount: 30,
+            vElementsCount: 30,
+        }
+    }
+
+    static getInstace() {
+        if (instance) return instance
+
+        instance = new AppDelegate()
+        return instance
+    }
+
+    getSizes() {
+        return this._sizes
+    }
+
+    init() {
+
+        this._initCanvas()
+        this._initResizeManager()
+    }
+
+    run() {
+        const loop = () => {
+
+
+
+            requestAnimationFrame(loop())
+        }
+        loop()
+    }
+
+    release() {
+
+        this._resizeObserver = null
+        this._canvas = null
+    }
+
+    _onResize() {
+        const canvasRect = this._canvas.getBoundingClientRect()
+
+        this._sizes.rectHeight = canvasRect.rectHeight / this._sizes.vElementsCount
+        this._sizes.rectWidth = canvasRect.rectWidth / this._sizes.hElementsCount
+    }
+
+    _initCanvas() {
+
+        this._canvas = document.getElementById("canvas__main")
+    }
+
+    _initResizeManager() {
+
+        this._resizeObserver = new ResizeObserver(() => {
+            console.log("resized")
+            this._onResize()
+        })
+
+        this._resizeObserver.observe(this._canvas)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict'
-import { GameManager } from "./gameManager.js"
-
-let instance
+import { AppDelegate } from "./appDelegate.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     AppDelegate.getInstace().init()
@@ -11,77 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('beforeunload', () => {
 
-    GameOfLife.getInstace().release()
+    AppDelegate.getInstace().release()
     instance = null
 })
-
-class AppDelegate {
-    _resizeObserver
-    _canvas
-    _sizes
-    _gameManager
-
-    constructor() {
-        this._resizeObserver = null
-        this._canvas = null
-        this._gameManager = new GameManager()
-        this._sizes = {
-            rectHeight: 0,
-            rectWidth: 0,
-            hElementsCount: 30,
-            vElementsCount: 30,
-        }
-    }
-
-    static getInstace() {
-        if (instance) return instance
-
-        instance = new AppDelegate()
-        return instance
-    }
-
-    init() {
-
-        this._initCanvas()
-        this._initResizeManager()
-    }
-
-    run() {
-        const loop = () => {
-
-
-
-            requestAnimationFrame(loop())
-        }
-        loop()
-    }
-
-    release() {
-
-        this._resizeObserver = null
-        this._canvas = null
-    }
-
-    _onResize() {
-        const canvasRect = this._canvas.getBoundingClientRect()
-
-        this._sizes.rectHeight = canvasRect.rectHeight / this._sizes.vElementsCount
-        this._sizes.rectWidth = canvasRect.rectWidth / this._sizes.hElementsCount
-    }
-
-    _initCanvas() {
-
-        this._canvas = document.getElementById("canvas__main")
-    }
-
-    _initResizeManager() {
-
-        this._resizeObserver = new ResizeObserver(() => {
-            console.log("resized")
-            this._onResize()
-        })
-
-        this._resizeObserver.observe(this._canvas)
-    }
-}
 
