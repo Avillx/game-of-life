9ef8b8af32fbad57ebec6d3ae5c9165602c2d0f9
Avill <for.sv01@gmail.com>
2026-01-19 13:52:38 +0500

    view and game manager integrated

--- src/appDelegate.js
+++ src/appDelegate.js
@@ -1,21 +1,21 @@
 import { GameManager } from "./gameManager.js"
+import { View } from "./view.js"
+import { COUNT_OF_COLUMNS, COUNT_OF_ROWS } from "./defines.js"
 
 let instance
 
 export class AppDelegate {
     _resizeObserver
     _canvas
-    _sizes
     _gameManager
+    _view
 
     constructor() {
         this._resizeObserver = null
         this._canvas = null
         this._gameManager = new GameManager()
-        this._sizes = {
-            rectHeight: 0,
-            rectWidth: 0
-        }
+        this._view = new View()
+
     }
 
     static getInstace() {
@@ -25,22 +25,26 @@ export class AppDelegate {
         return instance
     }
 
-    getSizes() {
-        return this._sizes
+    get2dContext() {
+
+        return this._canvas.getContext("2d");
     }
 
     init() {
 
         this._initCanvas()
         this._initResizeManager()
+        this._gameManager.init()
+        this._view.init(this.get2dContext(), this._gameManager.getField(), { rectHeight: null, rectWidth: null })
     }
 
     run() {
         const loop = () => {
 
+            this._gameManager.update()
+            this._view.render()
 
-
-            requestAnimationFrame(loop())
+            requestAnimationFrame(loop)
         }
         loop()
     }
@@ -54,8 +58,14 @@ export class AppDelegate {
     _onResize() {
         const canvasRect = this._canvas.getBoundingClientRect()
 
-        this._sizes.rectHeight = canvasRect.rectHeight / this._sizes.vElementsCount
-        this._sizes.rectWidth = canvasRect.rectWidth / this._sizes.hElementsCount
+        this._canvas.height = canvasRect.height
+        this._canvas.width = canvasRect.width
+        this._view.setRectSize({
+
+            rectHeight: canvasRect.height / COUNT_OF_COLUMNS,
+            rectWidth: canvasRect.width / COUNT_OF_ROWS
+        })
+
     }
 
     _initCanvas() {
@@ -66,6 +76,7 @@ export class AppDelegate {
     _initResizeManager() {
 
         this._resizeObserver = new ResizeObserver(() => {
+
             console.log("resized")
             this._onResize()
         })
